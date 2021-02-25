230075ab983e0468f6c564bbf2dfcf63a22be8f1
侯宇泽 <myrawaters59@gmail.com>
2021-02-25 13:17:30 +0800

    Update visual.py

--- visual.py
+++ visual.py
@@ -17,20 +17,22 @@ def chinese_jieba(text_str):
     return r_space_wordlist_str
 
 
-def word_cloud(text_str, path_png_str, path_font_str, png_name_str):
+def word_cloud(text_str, path_png_str, path_font_str, png_name_str, stop_words={}, scale_int=8):
     text_str=chinese_jieba(text_str)
     # 调用包PIL中的open方法，读取图片文件，通过numpy中的array方法生成数组
     mask_pic=np.array(Image.open(path_png_str))
     wordcloud = WordCloud(font_path=path_font_str,#设置字体
                         mask=mask_pic,#设置背景图片
                         background_color="white",#设置背景颜色
-                        max_font_size=150,# 设置字体最大值
-                        max_words=2000, # 设置最大显示的字数
-                        stopwords={'流浪地球', '星际穿越'}, #设置停用词，停用词则不再词云图中表示
+                        max_font_size=60,# 设置字体最大值
+                        max_words=100, # 设置最大显示的字数
+                        stopwords=stop_words, #设置停用词，停用词则不再词云图中表示
+                        scale=scale_int,    #设置分辨率（最大64）
                         ).generate(text_str)
     image=wordcloud.to_image()
     wordcloud.to_file(png_name_str)
     image.show()
+    print('word_cloud:DONE')
 
 
 if __name__ == '__main__':
